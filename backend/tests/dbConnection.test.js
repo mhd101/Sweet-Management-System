3f2c1f3d10a6c64710a35c4d70f41325f965e213
mhd101 <itzmohammadkhan@gmail.com>
2025-09-18 08:50:53 +0000

    test: add tests for db connection

---
+++ backend/tests/dbConnection.test.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import connectDB from "../src/config/db.js";
+
+jest.setTimeout(15000); // allow time for Atlas connection
+
+describe("MongoDB Connection", () => {
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  test("should connect to MongoDB Atlas successfully", async () => {
+    const conn = await connectDB();
+    expect(conn.connection.readyState).toBe(1); // 1 = connected
+  });
+
+
+});
